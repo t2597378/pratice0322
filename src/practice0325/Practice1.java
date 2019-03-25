df81cddd1e444b15a212df510de8b9cca1849621
t2597378 <t2597378@ulim.co>
2019-03-25 13:37:21 +0900

    add max-month

--- src/practice0325/Practice1.java
+++ src/practice0325/Practice1.java
@@ -13,7 +13,7 @@ public class Practice1 {
         System.out.println("9  10  11 12  13  14 ");
         System.out.println("15 16 17 18 19 20 21 ");
         System.out.println("22 23 24 25 26 27 28 ");
-        */
+
 
         //입력받은 두 수의 합을 구한다
 
@@ -38,6 +38,20 @@ public class Practice1 {
 
         System.out.printf("두 수의 합은 %d 입니다", result);
         scanner.close();
+        */
+
+        //숫자를 입력받아 해당하는 월의 최대 일 수를 출력하는 것.
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("달을 입력하세요.");
+        int month = scanner.nextInt();
+        int[] maxDays = {31,29,31,30,31,30,31,31,30,31,30,31};
+
+        System.out.printf("해당 달의 최대 일수는 %d입니다.", maxDays[month-1]);
+
+        scanner.close();
+
 
     }
 
